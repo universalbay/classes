154a41d174c0e64fda61aa0800b07a2db0419e7d
BHeart <bh34rt@gmail.com>
2013-11-14 19:45:36 +0100

    Added javadoc for CharacterClass#getName()

--- src/main/java/com/universalbay/classes/CharacterClass.java
+++ src/main/java/com/universalbay/classes/CharacterClass.java
@@ -6,6 +6,11 @@ import org.bukkit.inventory.ItemStack;
  * Represents a type of Class that a {@link HumanEntity} can be assigned.
  */
 public interface CharacterClass {
+    /**
+     * Gets the name of this {@code CharacterClass}.
+     *
+     * @return  a String containing the name of this {@code CharacterClass}
+     */
     String getName();
 
     ItemStack getIcon();
