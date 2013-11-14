354c13eaeaa930eab23256ac30761a38e02d6ae0
BHeart <bh34rt@gmail.com>
2013-11-14 19:45:27 +0100

    Added javadoc for CharacterClass

--- src/main/java/com/universalbay/classes/CharacterClass.java
+++ src/main/java/com/universalbay/classes/CharacterClass.java
@@ -2,6 +2,9 @@ package com.universalbay.classes;
 
 import org.bukkit.inventory.ItemStack;
 
+/**
+ * Represents a type of Class that a {@link HumanEntity} can be assigned.
+ */
 public interface CharacterClass {
     String getName();
 
